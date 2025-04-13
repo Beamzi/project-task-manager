4b4fd311a371a1975b92c26ba633f67e4f53d788
James Day <159420270+Beamzi@users.noreply.github.com>
2025-04-13 12:25:05 +1000

    added auth and sign in sign out comps

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
 import AllTasks from "@/components/AllTasks";
+import SignInBtn from "@/components/buttons/SignInBtn";
 import TasksProvider from "@/components/TasksProvider";
 import { prisma } from "@/lib/prisma";
 import React from "react";
+import { auth } from "@/auth";
+import { SignOutBtn } from "@/components/buttons/SignOutBtn";
 
 async function getTasks() {
   const tasks = await prisma.task.findMany({
@@ -16,12 +19,16 @@ async function getTasks() {
 }
 
 export default async function AllTasksView() {
+  const session = await auth();
+  console.log({ session });
   const tasks = await getTasks();
   console.log(tasks);
   return (
     <TasksProvider tasks={tasks}>
       <main className="border-2 p-5 ml-5 w-[80%]">
         <div>THIS IS TASK VIEW PAGE</div>
+        <SignInBtn></SignInBtn>
+        <SignOutBtn></SignOutBtn>
         <AllTasks></AllTasks>
       </main>
     </TasksProvider>
